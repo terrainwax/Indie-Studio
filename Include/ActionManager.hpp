c3636667b37eb895f703072e227b1112e494b78e
Reimua <thibault.dolley@gmail.com>
2018-05-15 13:09:37 +0000

    woopsi

---
+++ Include/ActionManager.hpp
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2018
+**  <------------>
+** File description:
+**    <------->
+*/
+
+#pragma once
+
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#include <irrlicht.h>
+#include <Keycodes.h>
+#include <IEventReceiver.h>
+#pragma GCC diagnostic warning "-Wunused-parameter"
+
+#include "driverChoice.h"
+
+class ActionManager final : public irr::IEventReceiver {
+public:
+	ActionManager();
+	~ActionManager() override;
+
+	bool isKeyUp(irr::EKEY_CODE key);
+	bool isKeyDown(irr::EKEY_CODE key);
+	bool isKeyPressed(irr::EKEY_CODE key);
+	bool isKeyReleased(irr::EKEY_CODE key);
+	virtual bool OnEvent(const irr::SEvent& event) override;
+	void flush();
+private:
+	bool _currentState[irr::KEY_KEY_CODES_COUNT];
+	bool _previousState[irr::KEY_KEY_CODES_COUNT];
+};
