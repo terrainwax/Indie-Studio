22de80ca89b159f3151b64cff53d709c5c74b6a4
Reimua <thibault.dolley@gmail.com>
2018-05-31 15:45:05 +0000

    Added bombKey to keyset

--- Src/KeySet.cpp
+++ Src/KeySet.cpp
@@ -7,11 +7,12 @@
 
 #include "KeySet.hpp"
 
-KeySet::KeySet(irr::EKEY_CODE up, irr::EKEY_CODE down, irr::EKEY_CODE left, irr::EKEY_CODE right) :
-keyUp(up),
-keyDown(down),
-keyLeft(left),
-keyRight(right)
+KeySet::KeySet(irr::EKEY_CODE up, irr::EKEY_CODE down, irr::EKEY_CODE left, irr::EKEY_CODE right, irr::EKEY_CODE bomb) :
+upKey(up),
+downKey(down),
+leftKey(left),
+rightKey(right),
+bombKey(bomb)
 {
 }
 
