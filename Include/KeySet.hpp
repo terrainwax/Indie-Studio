874b6398af4a277122328d7faa248a1ac659798e
Reimua <thibault.dolley@gmail.com>
2018-05-31 15:45:14 +0000

    Added bombKey to keyset

--- Include/KeySet.hpp
+++ Include/KeySet.hpp
@@ -11,14 +11,16 @@
 
 
 class KeySet final {
+public:
 	KeySet(irr::EKEY_CODE up = irr::KEY_UP,
 		irr::EKEY_CODE down = irr::KEY_DOWN,
 		irr::EKEY_CODE left = irr::KEY_LEFT,
-		irr::EKEY_CODE right = irr::KEY_RIGHT);
+		irr::EKEY_CODE right = irr::KEY_RIGHT,
+		irr::EKEY_CODE bomb = irr::KEY_SPACE);
 	~KeySet();
-public:
-	irr::EKEY_CODE keyUp;
-	irr::EKEY_CODE keyDown;
-	irr::EKEY_CODE keyLeft;
-	irr::EKEY_CODE keyRight;
+	irr::EKEY_CODE upKey;
+	irr::EKEY_CODE downKey;
+	irr::EKEY_CODE leftKey;
+	irr::EKEY_CODE rightKey;
+	irr::EKEY_CODE bombKey;
 };
