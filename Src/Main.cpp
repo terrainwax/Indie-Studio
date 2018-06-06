315b986b294d9f0b69fda07d247b231abe38002e
Swann JAUNASSE <swann.jaunasse@epitech.eu>
2018-06-06 19:23:19 +0200

    Menu Branch

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -15,6 +15,37 @@
 
 int main(int argc, char **argv)
 {
+	irr::IrrlichtDevice* device = irr::createDevice(irr::video::EDT_OPENGL, irr::core::dimension2d<irr::u32>(640,480), 32, false, false, false);
+	irr::video::IVideoDriver* driver = device->getVideoDriver();
+	irr::scene::ISceneManager *sceneManager = device->getSceneManager();
+
+	irr::video::ITexture *image = driver->getTexture("Assets/Textures/Bomberman.png");    // chargement de l'image
+	driver->makeColorKeyTexture (image, irr::core::position2d<irr::s32> (0,0)); // transparence pour le fond
+
+	irr::video::SColor blanc;                                          // creation variable couleur blanche
+	blanc.set(255,255,255,255);
+
+	while(device->run ())                                              // la boucle de rendu
+	{
+	driver->beginScene(true, true,
+		irr::video::SColor (0,120,120,120));
+	driver->draw2DImage(image,                                     // dessin de la salle
+		irr::core::position2d<irr::s32> (20,20),
+		irr::core::rect<irr::s32> (0,0,342,224),
+		0, blanc, true);
+	driver->draw2DImage (image,                                    // dessin du premier truc rouge
+		irr::core::position2d<irr::s32> (140,140),
+		irr::core::rect<irr::s32> (349,15,385,78),
+		0, blanc, true);
+	driver->draw2DImage (image,                                    // dessin du deuxième truc rouge
+		irr::core::position2d<irr::s32> (150,20),
+		irr::core::rect<irr::s32> (387,15,423,78),
+		0, blanc, true);
+		driver->endScene ();
+	}
+
+	device->drop ();
+	/*
 	(void)argc;
 	(void)argv;
 	srand(time(NULL));
@@ -32,6 +63,6 @@ int main(int argc, char **argv)
 		game.display(graphic);
 		game.update(action, graphic, sound);
 		graphic.render();
-	}
+	}*/
 	return 0;
 }
