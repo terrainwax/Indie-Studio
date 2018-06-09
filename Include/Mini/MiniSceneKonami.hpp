600415d3f216e9662231a83e97cf0ecd1da61730
Swann JAUNASSE <swann.jaunasse@epitech.eu>
2018-06-09 18:43:35 +0200

    [Auto Commit]

--- Include/Mini/MiniSceneKonami.hpp
+++ Include/Mini/MiniSceneKonami.hpp
@@ -24,4 +24,5 @@ public:
 
 private:
 	MiniSprite _back;
+	MiniSprite _player;
 };
--- Src/Mini/MiniSceneKonami.cpp
+++ Src/Mini/MiniSceneKonami.cpp
@@ -28,6 +28,11 @@ void MiniSceneKonami::start(IMiniCore *core, IMiniAudioMgr *audio, IMiniVideoMgr
 	_back = MiniSprite(video->loadTexture("Assets/Sprites/MiniGameBackground.jpg"));
 	_back.destination.width = video->getScreenWidth();
 	_back.destination.height = video->getScreenHeight();
+
+	_player = MiniSprite(video->loadTexture("Assets/Sprites/Bomberman.png"));
+
+	_player.destination.width *= 4;
+	_player.destination.height *= 4;
 }
 
 void MiniSceneKonami::stop(IMiniCore *core, IMiniAudioMgr *audio, IMiniVideoMgr *video)
@@ -44,8 +49,24 @@ void MiniSceneKonami::updateFrame(IMiniCore *core, IMiniActionMgr *action, IMini
 	(void)audio;
 	(void)clock;
 
-	if (action->isKeyPressed(irr::KEY_ESCAPE))
+	if (action->isKeyPressed(irr::KEY_ESCAPE)) {
+		audio->clear();
+		audio->playMusic(MENU_MUSIC);
 		core->pop();
+	}
+
+	if (action->isKeyPressed(irr::KEY_UP)) {
+		_player.destination.y -= 10;
+	}
+	else if (action->isKeyPressed(irr::KEY_DOWN)) {
+		_player.destination.y += 10;
+	}
+	else if (action->isKeyPressed(irr::KEY_LEFT)) {
+		_player.destination.x -= 10;
+	}
+	else if (action->isKeyPressed(irr::KEY_RIGHT)) {
+		_player.destination.x += 10;
+	}
 }
 
 void MiniSceneKonami::renderFrame(IMiniCore *core, IMiniVideoMgr *video, IMiniAudioMgr *audio, const Clock &clock)
@@ -56,4 +77,5 @@ void MiniSceneKonami::renderFrame(IMiniCore *core, IMiniVideoMgr *video, IMiniAu
 	(void)clock;
 
 	video->drawSprite(_back);
+	video->drawSprite(_player);
 }
